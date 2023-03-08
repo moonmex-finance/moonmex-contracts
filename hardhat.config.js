2156d9e3c2056fd09265318756c505e55041766b
caoyu <570366997@qq.com>
2023-03-08 23:15:47 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,25 +4,10 @@ require("hardhat-contract-sizer")
 require('@typechain/hardhat')
 
 const {
-  BSC_URL,
-  BSC_DEPLOY_KEY,
-  BSCSCAN_API_KEY,
-  POLYGONSCAN_API_KEY,
-  SNOWTRACE_API_KEY,
-  ARBISCAN_API_KEY,
-  ETHERSCAN_API_KEY,
-  BSC_TESTNET_URL,
-  BSC_TESTNET_DEPLOY_KEY,
-  ARBITRUM_TESTNET_DEPLOY_KEY,
-  ARBITRUM_TESTNET_URL,
-  ARBITRUM_DEPLOY_KEY,
-  ARBITRUM_URL,
-  AVAX_DEPLOY_KEY,
-  AVAX_URL,
-  POLYGON_DEPLOY_KEY,
-  POLYGON_URL,
-  MAINNET_URL,
-  MAINNET_DEPLOY_KEY
+  MATIC_TESTNET_URL,
+  MATIC_TESTNET_DEPLOY_KEY,
+  MOONBEAM_URL,
+  MOONBEAM_DEPLOY_KEY
 } = require("./env.json")
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -43,61 +28,23 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   networks: {
-    localhost: {
-      timeout: 120000
-    },
-    hardhat: {
-      allowUnlimitedContractSize: true
-    },
-    bsc: {
-      url: BSC_URL,
-      chainId: 56,
-      gasPrice: 10000000000,
-      accounts: [BSC_DEPLOY_KEY]
-    },
-    testnet: {
-      url: BSC_TESTNET_URL,
-      chainId: 97,
+    maticTestnet: {
+      url: MATIC_TESTNET_URL,
+      chainId: 80001,
       gasPrice: 20000000000,
-      accounts: [BSC_TESTNET_DEPLOY_KEY]
+      accounts: [MATIC_TESTNET_DEPLOY_KEY]
     },
-    arbitrumTestnet: {
-      url: ARBITRUM_TESTNET_URL,
-      gasPrice: 10000000000,
-      chainId: 421611,
-      accounts: [ARBITRUM_TESTNET_DEPLOY_KEY]
-    },
-    arbitrum: {
-      url: ARBITRUM_URL,
-      gasPrice: 30000000000,
-      chainId: 42161,
-      accounts: [ARBITRUM_DEPLOY_KEY]
-    },
-    avax: {
-      url: AVAX_URL,
-      gasPrice: 200000000000,
-      chainId: 43114,
-      accounts: [AVAX_DEPLOY_KEY]
-    },
-    polygon: {
-      url: POLYGON_URL,
+    moonbeam: {
+      url: MOONBEAM_URL,
+      chainId: 1284,
       gasPrice: 100000000000,
-      chainId: 137,
-      accounts: [POLYGON_DEPLOY_KEY]
-    },
-    mainnet: {
-      url: MAINNET_URL,
-      gasPrice: 50000000000,
-      accounts: [MAINNET_DEPLOY_KEY]
+      accounts: [MOONBEAM_DEPLOY_KEY]
     }
   },
   etherscan: {
     apiKey: {
-      mainnet: MAINNET_DEPLOY_KEY,
-      arbitrumOne: ARBISCAN_API_KEY,
-      avalanche: SNOWTRACE_API_KEY,
-      bsc: BSCSCAN_API_KEY,
-      polygon: POLYGONSCAN_API_KEY,
+      maticTestnet: MATIC_TESTNET_DEPLOY_KEY,
+      moonbeam: MOONBEAM_DEPLOY_KEY,
     }
   },
   solidity: {
